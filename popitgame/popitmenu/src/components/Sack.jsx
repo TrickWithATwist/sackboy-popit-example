856b5fb788b76ee98b3054ced901cc97962abfee
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2024-08-06 01:14:53 -0400

    small changes

--- popitgame/popitmenu/src/components/Sack.jsx
+++ popitgame/popitmenu/src/components/Sack.jsx
@@ -1,8 +1,15 @@
 import '../App.css';
+import './Sack.css';
+import image1 from './assets/Sackboy_Game_Model.png';
 
 export default function Sack()
 {
     return(
-        <h1>hello world</h1>
+        <div className='flexbox'>
+            <img classname='sack1'src={image1} alt="" />
+            <img classname='sack2'src={image1} alt="" />
+            <img classname='sack3'src={image1} alt="" />
+            <img classname='sack4'src={image1} alt="" />
+        </div>
     )
 }
