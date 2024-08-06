7e09c4dd93b1b215fb8b3e79552cbb30e88a52d2
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2024-08-05 22:24:27 -0400

    initial commit

---
+++ popitgame/popitmenu/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Sack from './components/Sack';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Sack/>
+    </div>
+  );
+}
+
+export default App;
---
+++ popitgame/popitmenu/src/components/Sack.jsx
@@ -0,0 +1,8 @@
+import '../App.css';
+
+export default function Sack()
+{
+    return(
+        <h1>hello world</h1>
+    )
+}
