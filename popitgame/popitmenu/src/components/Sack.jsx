9fa70ff5f42bb2207ca907c6d2709d1f2dc3f71d
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2024-08-06 20:25:18 -0400

    adding popit items

--- popitgame/popitmenu/src/components/Sack.jsx
+++ popitgame/popitmenu/src/components/Sack.jsx
@@ -1,13 +1,16 @@
 import '../App.css';
 import './Sack.css';
 import image1 from './assets/Sackboy_Game_Model.png';
+import sackidle from './assets/smileidle.png';
+import sackhover from './assets/smilehover.png'
+import cursor from './assets/cursor.gif'
 
 export default function Sack()
 {
     return(
         <div className='flexbox'>
-            <img classname='sack1'src={image1} alt="" />
-            <img classname='sack2'src={image1} alt="" />
+            <div className='sack1'></div>
+            <div className='cursor'></div>
             <img classname='sack3'src={image1} alt="" />
             <img classname='sack4'src={image1} alt="" />
         </div>
