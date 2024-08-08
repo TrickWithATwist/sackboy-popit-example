adfc91465415d47ba4d95ff503147db92f204fde
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2024-08-07 23:14:11 -0400

    adding the tool tip

--- popitgame/popitmenu/src/components/Sack.jsx
+++ popitgame/popitmenu/src/components/Sack.jsx
@@ -10,7 +10,9 @@ export default function Sack()
     return(
         <div className='flexbox'>
             <div className='layer1'>
-                <div className='sack1'></div>
+                <div className='sack1'>
+                    <span className='tooltip'>Costume</span>
+                </div>
             </div>
             <div className='layer2'>
                 <div className='cursor'></div>
