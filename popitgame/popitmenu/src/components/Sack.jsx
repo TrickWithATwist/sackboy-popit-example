a2b3d8228fbc766900338adc7e976a704e570ec4
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2024-08-16 18:31:46 -0400

    added array for themes and functions for change

--- popitgame/popitmenu/src/components/Sack.jsx
+++ popitgame/popitmenu/src/components/Sack.jsx
@@ -6,10 +6,40 @@ import sackhover from './assets/smilehover.png'
 import cursor from './assets/cursor.gif'
 import Modal from './Modal';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Sack()
 {
     const [open, setopen] = useState(false)
+
+    //functions for themes
+    const [current_theme, setcurrent_theme] = useState('default')
+
+    const themes = {
+        default: { background: 'black', tooltip: 'black' },
+        coolisland: {background:'linear-gradient(0deg, rgba(185,234,107,1) 46%, rgba(99,178,220,1) 94%)', tooltip: '#63b2dc'},
+        icecream: {background: 'linear-gradient(0deg, rgba(224,206,78,1) 46%, rgba(223,93,196,1) 94%)', tooltip: '#DF5DC4'},
+        candyjar: {background: 'linear-gradient(0deg, rgba(246,19,73,1) 37%, rgba(91,25,57,1) 85%)', tooltip: '#5b1939'},
+        forestsurprise: {background: 'linear-gradient(0deg, rgba(221,0,222,1) 9%, rgba(4,2,3,1) 90%)', tooltip:'black'},
+        slicedbread: {background:'linear-gradient(0deg, rgba(248,230,231,1) 32%, rgba(201,212,139,1) 77%)', tooltip:'#c9d48b'}
+    }
+
+    const handleThemeChange = (themeName) => {
+        setcurrent_theme(themeName);
+        setopen(false);
+    };
+
+    useEffect(() => {
+        const theme = themes[current_theme];
+        document.documentElement.style.setProperty('--background-color', theme.background);
+        document.documentElement.style.setProperty('--tooltip-bg', theme.tooltip);
+        document.documentElement.style.setProperty('--tooltip-color', theme.background);
+    }, [current_theme]);
+
+
+
+
+
     return(
         <div className='flexbox'>
             <div className='layer1'>
